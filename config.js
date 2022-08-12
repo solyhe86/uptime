aa6c369878607e96ad85ef5ae284598fde4e06cc
solyhe86 <108418679+solyhe86@users.noreply.github.com>
2022-08-12 23:12:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ window.Config = {
   CountDays: 60,
 
   // 是否显示检测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 导航栏菜单
   Navi: [
@@ -27,7 +27,7 @@ window.Config = {
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://blog.xhuman.tk/'
     },
   ],
 };
